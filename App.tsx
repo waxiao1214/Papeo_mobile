3a9cdae1777371c80150cc8e0c8831088639c773
xiaowang <wxiao1214@gmail.com>
2021-02-03 00:54:58 -0800

    Completed the enter your email screen

--- App.tsx
+++ App.tsx
@@ -8,14 +8,14 @@
 
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-// import Login from './src/screens/login/index';
-import TermsConditions from './src/screens/TermsCondition';
+import Login from './src/screens/login/index';
+// import TermsConditions from './src/screens/TermsCondition';
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-        {/* <Login /> */}
-        <TermsConditions />
+        <Login />
+        {/* <TermsConditions /> */}
     </>
   );
 };
--- src/screens/login/EnterEmail.tsx
+++ src/screens/login/EnterEmail.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Button from '../../components/Button';
+import Colors from '../../styles/colors/colors';
+import ReturnButton from '../../components/ReturnButton';
+import InputText from '../../components/TextInput';
 
-const EmailScreen = () => {
+const EnterEmailScreen = () => {
   return (
-    <View>
-      <Text>Email Screen</Text>
-    </View>
+    <>
+      <View style={{marginTop: 30}}>
+        <InputText />
+      </View>
+      <View style={{marginTop: 25}}>
+        <Button title='Continue' bgColor={Colors.$primary} color='white' onPress={() => {}}/>
+      </View>
+      <View style={{position: 'absolute', top: -200}}>
+        <ReturnButton onClick={() => {}}/>
+      </View>
+    </>
   )
 }
 
-export default EmailScreen;
+export default EnterEmailScreen;
