353e7a3c1db098e10bfd04b3f164b201acfbda3a
YongLin Qiu <typescript59@gmail.com>
2021-02-03 06:21:10 +0100

    [New] Completed the create-party-privacy widget.

--- src/screens/create-party/components/render-widgets/render-widgets.tsx
+++ src/screens/create-party/components/render-widgets/render-widgets.tsx
@@ -3,6 +3,7 @@ import RenderWidgetsProps from './render-widgets.interface';
 import CreatePartyDescriptionWidget from '../../widgets/create-party-description/create-party-description.widget';
 import CreatePartyNameWidget from '../../widgets/create-party-name/create-party-name.widget';
 import CreatePartyTypeWidget from '../../widgets/create-party-type/create-party-type.widget';
+import CreatePartyPrivacyWidget from '../../widgets/create-party-privacy/create-party-privacy.widget';
 
 const RenderWidgets: React.FC<RenderWidgetsProps> = ({ step }) => {
   switch (step) {
@@ -12,6 +13,8 @@ const RenderWidgets: React.FC<RenderWidgetsProps> = ({ step }) => {
       return <CreatePartyDescriptionWidget />;
     case 3:
       return <CreatePartyTypeWidget />;
+    case 4:
+      return <CreatePartyPrivacyWidget />;
     default:
       return null;
   }
